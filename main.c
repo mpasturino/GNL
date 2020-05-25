7122c2b04cfa71dcfb7f801540bf9934b4cff70e
Marco Pasturino <marco.pasturino@gmail.com>
2020-05-25 16:52:16 -0300

    Evolucao 2

--- main.c
+++ main.c
@@ -1,24 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpasturi <mpasturi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/19 19:42:13 by mpasturi          #+#    #+#             */
+/*   Created: 2020/05/25 16:04:15 by mpasturi          #+#    #+#             */
 /*   Updated: 2020/05/25 16:52:08 by mpasturi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 #include "get_next_line.h"
+#include "get_next_line.c"
 
-int     BUFFER_SIZE = 32;
-
-int get_next_line(int fd, char **line)
+int     main()
 {
-
-    if (line == 0 || fd < 0 || BUFFER_SIZE == 0)
-        return (-1);
+    int     fd;
+    char    buf[400];
+    char    **line[4000];
     
-}
+    fd = open("texto_1.rft", O_RDONLY);
+    if (fd == -1)
+    {
+        exit(1);
+    }
+    get_next_line(fd, **line);
 
+}
